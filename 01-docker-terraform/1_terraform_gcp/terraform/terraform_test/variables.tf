268ba13090613d1cd73d2a3bc01d1f7599d4100c
yeltayzhastay <54392243+yeltayzhastay@users.noreply.github.com>
2024-01-30 03:07:37 +0600

    terraform

--- 01-docker-terraform/1_terraform_gcp/terraform/terraform_test/variables.tf
+++ 01-docker-terraform/1_terraform_gcp/terraform/terraform_test/variables.tf
@@ -1,5 +1,29 @@
+variable "credentials" {
+  description = "My Credentials"
+  default     = "C:/Users/yelzha/AppData/Roaming/gcloud/de-zoomcamp-412720-a3b1e76e7cf1.json"
+}
+
 variable "project" {
-  description = "de-zoomcamp-yelzha"
-  default = "de-zoomcamp"
   type = string
+  default = "de-zoomcamp-412720"
+}
+
+variable "region" {
+  type = string
+  default = "us-central1"
+}
+
+variable "gcs_bucket_name" {
+  type = string
+  default = "de-zoomcamp-bucket-yelzha"
+}
+
+variable "location" {
+  type = string
+  default = "US"
+}
+
+variable "bq_dataset_name" {
+  type = string
+  default = "zoomcamp_dataset"
 }
